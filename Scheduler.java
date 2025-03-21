8709fdefb42b571557ddacf5f9be734e943d8c3a
ezanakg <ezanakgebru@gmail.com>
2025-03-21 18:59:25 -0400

    Finished implementing the scheduler and processes.

--- Scheduler.java
+++ Scheduler.java
@@ -106,3 +106,18 @@ public class Scheduler {
     }
 }
 
+class Process {
+    int pid;
+    int arrivalTime;
+    int burstTime;
+    int priority;
+    int waitingTime = 0;
+    int turnaroundTime = 0;
+
+    public Process(int pid, int arrivalTime, int burstTime, int priority) {
+        this.pid = pid;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
+}
